c64361ab0e8f30f9ccef8665f5f39fc9323dcd3e
masdefry <muhammaddefryan@gmail.com>
2022-01-24 21:25:25 +0700

    Day-01

---
+++ Comment.js
@@ -0,0 +1,24 @@
+// COMMENT
+// Fungsi-1 : Untuk komunikasi antar developer
+// Fungsi-2 : Untuk note bagi kita sendiri
+
+// Ex.
+// Login Page
+// ...
+// ...
+// ...
+
+// Register Page
+// ...
+// ...
+// ...
+
+// Ex. Comment Diabaikan oleh Mesin
+// Comment-1
+console.log('Hello World!')
+
+// Comment-2
+console.log('Hai Dunia!')
+
+// Comment-3
+console.log('Hello Purwadhika!') // Code untuk menyapa student Purwadhika
---
+++ ExternalJavascript.js
@@ -0,0 +1,2 @@
+console.log('Hello External File!')
+console.log(10)
