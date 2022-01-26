6efc2fae146f54c711918f6fd72a0849366e6394
masdefry <muhammaddefryan@gmail.com>
2022-01-26 17:00:14 +0700

    Day-03

--- ComparisonLogicalOperator.js
+++ ComparisonLogicalOperator.js
@@ -25,3 +25,33 @@ let angka8 = 10
 let angka9 = `${angka7}` // Dianggap sebagai sebuah string
 console.log(angka8 === angka9)
 // Note: String dapat menyimpan 2 bentuk data, alfabet dan numeric
+
+let text1 = 'Aku'
+let text2 = 'aku'
+console.log(text1 == text2)
+
+// LOGICAL OPERATOR ---> Digunakan untuk compare lebih dari 2 kondisi
+// - && (AND)
+// - || (OR)
+// - ! (NOT)
+
+// && (AND) ---> Salah 1 kondisi bernilai false, maka hasil akhir akan false
+//          ---> Dua kondisi harus bernilai true apabila ingin hasil akhir true
+
+let beratBadan = 65
+console.log(beratBadan > 50 && beratBadan < 90) // true && true ---> true
+console.log(beratBadan > 65 && beratBadan < 66) // false && true ---> false
+
+// || (OR) ---> Salah 1 kondisi bernilai true, maka hasil akhir akan true
+//         ---> Dua kondisi harus bernilai false apabila ingin hasil akhir false
+let nilaiModul = 85
+console.log(nilaiModul > 70 || nilaiModul < 90) // true || true ---> true
+console.log(nilaiModul >= 84 || nilaiModul < 85) // true || false ---> true
+
+// ! (NOT)
+let tinggiBadan = 170
+let result = tinggiBadan > 160 || tinggiBadan < 170 // true || false ---> true
+console.log(!result)
+
+let resultFalse = false 
+console.log(!resultFalse)
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Hello, JCWDL-001!
 - Date Object
-- Comparison & Logical Operator
+- Comparison & Logical Operator ---> Menghasilkan Nilai Boolean (True/False Saja)
+    - Comparison : >, >=, <, <=, ==, ===, !=
+    - Logical : && (AND), || (OR), !(NOT)
 - Conditional Statement
+    - IF
+    - IF ELSE
+    - IF, ELSE IF, ELSE
+    - SWITCH CASE
