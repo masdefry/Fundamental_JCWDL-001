148971cf077880e1b801642c3e8d82cd03085e75
masdefry <muhammaddefryan@gmail.com>
2022-02-09 21:33:42 +0700

    Day-11

---
+++ DatabaseProducts.js
@@ -0,0 +1,27 @@
+// ### Database Products
+let dataProducts = [
+    { 
+        name: 'Lenovo Ideapad Gaming', 
+        price: 15000000,
+        stock: 100,
+        image: 'https://1.bp.blogspot.com/-B1be0mW1Jzo/Xviyt1Z9L8I/AAAAAAAAfXw/nFx593OCTZgfPr8N7sUTlTeMG8VUG41bACK4BGAsYHg/s828/Lenovo%2BIdeaPad%2BGaming%2B3i%2B15IMH05%2BLaptophia.JPG' 
+    },
+    {
+        name: 'ASUS TUF',
+        price: 12500000,
+        stock: 50, 
+        image: 'https://img.tek.id/img/content/2019/07/23/18559/tuf-gaming-fx505-dt-laptop-gaming-murah-yang-hampir-sempurna-Lx7LHkyT4i.jpg'
+    },
+    {
+        name: 'Acer Nitro',
+        price: 17500000,
+        stock: 25, 
+        image: 'https://s0.bukalapak.com/img/5052238772/large/Laptop_Acer_Predator_Nitro_5.jpg.webp'
+    },
+    {
+        name: 'Acer Nitro',
+        price: 17500000,
+        stock: 25, 
+        image: 'https://s0.bukalapak.com/img/5052238772/large/Laptop_Acer_Predator_Nitro_5.jpg.webp'
+    },
+]
---
+++ FunctionMarket.js
@@ -0,0 +1,79 @@
+// ### Show Data Products
+function showDataProducts(){
+    let output = ''
+    dataProducts.forEach((value, index) => {
+        output += `
+            <tr>
+                <td>
+                    ${index}
+                </td>
+                <td>
+                    ${value.name}
+                </td>
+                <td>
+                    ${value.price}
+                </td>
+                <td>
+                    ${value.stock}
+                </td>
+                <td>
+                    <image src="${value.image}" width="100px" />
+                </td>
+                <td>
+                    <input type="button" value="Edit">
+                    <input type="button" value="Delete" onClick="DeleteDataProduct(${index})">
+                </td>
+            </tr>
+        `
+    })
+
+    let getTable = document.getElementById('tableProducts').getElementsByTagName('tbody')[0]
+    getTable.innerHTML = output
+}
+showDataProducts()
+
+// ### Add Data Product
+document.getElementById('buttonSubmit').addEventListener('click', AddDataProduct)
+function AddDataProduct(){
+    let inputs = document.getElementsByClassName('inputProduct')
+    
+    let productName = inputs[0].value 
+    let productPrice = inputs[1].value 
+    let productStock = inputs[2].value 
+    let productImage = inputs[3].value 
+
+    if(productName && productPrice && productStock && productImage){
+        // Push Datanya
+        dataProducts.push(
+            {
+                name: productName,
+                price: productPrice,
+                stock: productStock,
+                image: productImage
+            }
+        )
+
+        showDataProducts()
+
+        inputs[0].value = ''
+        inputs[1].value = ''
+        inputs[2].value = ''
+        inputs[3].value = ''
+    }else{
+        alert('Semua Data Harus Diisi!')
+    }
+}
+
+// ### Delete Data Product
+function DeleteDataProduct(index){
+    let confirmBox = confirm(`
+        Are you sure want to delete ${dataProducts[index].name}?
+    `)
+
+    if(confirmBox){ // confirmBox === true
+        // Delete Data
+        dataProducts.splice(index, 1)
+        alert('Delete Product Success!')
+        showDataProducts()
+    }
+}
