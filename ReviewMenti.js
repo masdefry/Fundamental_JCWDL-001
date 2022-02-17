4372d3aaecdee86aa602a0be1554b1f42131e02e
masdefry <muhammaddefryan@gmail.com>
2022-02-17 09:25:21 +0700

    Day-14

---
+++ ReviewMenti.js
@@ -0,0 +1,28 @@
+// let println = 'Hello'
+
+// if(parseInt('100XYZ') === 100){
+//     println = ', World!'
+// }
+
+// console.log(println)
+
+let point = 10
+let isTrue
+
+let mie = [1, 2]
+console.log(...mie)
+
+// let klub = {
+//     klub1: 'Bali',
+//     klub2: 'Persib'
+// }
+
+// console.log(...klub)
+
+
+let arr = [3.5, 7.2, 5.5, 9.9, 2.3]
+let a = arr.map(value => Math.round(value))
+let b = arr.filter(value => value < 5.5)
+console.log(a)
+console.log(b)
+console.log(arr)
